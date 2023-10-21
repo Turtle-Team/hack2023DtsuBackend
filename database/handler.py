50466064eff15ad52defc480e0d2ae66355db02a
BeesKnight <home@BeesKnight.local>
2023-10-21 09:11:46 +0300

    сделал дела с квизами

--- database/handler.py
+++ database/handler.py
@@ -19,6 +19,18 @@ class Db:
                                                   password=settings.DATABASE['password'])
         self.cursor = self.connection.cursor()
 
+#это определенные квизы, но он выдает сразу все в виде кортежей
+    def select_quiz(self, quiz_name):
+        try:
+            query = "SELECT * FROM quiz WHERE name = %s"
+            self.cursor.execute(query, (quiz_name,))  # передаем имя квиза как параметр
+
+            result = self.cursor.fetchall()
+
+            return result
+        except mysql.connector.Error as err:
+            print("Error: {}".format(err))
+            return None
 
     def __del__(self):
         self.connection.commit()
