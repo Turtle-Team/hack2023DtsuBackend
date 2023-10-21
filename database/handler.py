503425ccdaa84500d8455c0d61a97df73f362aee
BeesKnight <home@BeesKnight.local>
2023-10-21 09:55:24 +0300

    проверка на выполнение квизов

--- database/handler.py
+++ database/handler.py
@@ -19,7 +19,24 @@ class Db:
                                                   password=settings.DATABASE['password'])
         self.cursor = self.connection.cursor()
 
-#это определенные квизы, но он выдает сразу все в виде кортежей
+    def check_quiz_completion(self, username, quiz_id):
+        try:
+            query = "SELECT is_complete FROM users WHERE login = %s AND quiz_to = %s"
+            self.cursor.execute(query, (username, quiz_id))
+
+            result = self.cursor.fetchone()
+
+            if result and result[0]:
+                print("Пользователь прошел квиз")
+                return True
+            else:
+                print("Пользователь не прошел квиз")
+                return False
+        except mysql.connector.Error as err:
+            print("Error: {}".format(err))
+            return False
+
+    #это определенные квизы, но он выдает сразу все в виде кортежей
     def select_quiz(self, quiz_name):
         try:
             query = "SELECT * FROM quiz WHERE name = %s"
